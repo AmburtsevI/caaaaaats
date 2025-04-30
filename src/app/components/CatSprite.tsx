109636421a30ccef58660fc30e45863a9fd00e6b
IgorAmburtsev <138751890+IgorAmburtsev@users.noreply.github.com>
2025-05-01 00:39:53 +0300

    Cats starter

--- src/app/components/CatSprite.tsx
+++ src/app/components/CatSprite.tsx
@@ -7,22 +7,23 @@ export default function CatSprite() {
     const pixiSprite = useRef<Sprite>(null);
 
     const [texture, setTexture] = useState(Texture.EMPTY)
-    const [x, setX] = useState(550)
+    const [x, setX] = useState(50)
     const [reversed, setReversed] = useState(false);
 
     const speed = 2
 
     useTick(() => {
+        console.log(x)
         if (x === 796) {
-            setReversed(true)
-            console.log(x, reversed)
+            // setReversed(true)
+            // console.log(x, reversed)
         }
         if (x === 550) {
             setReversed(false)
         }
         
         if (reversed) {
-            setX((prev) => (prev - speed) % 800);
+            // setX((prev) => (prev - speed) % 800);
         } else {
             setX((prev) => (prev + speed) % 800);
         }
@@ -43,10 +44,10 @@ export default function CatSprite() {
     return (
         <pixiSprite ref={pixiSprite} 
                     texture={texture}
-                    anchor={{ x: .5, y: 0}} 
+                    anchor={{ x: 0.5, y: 0.5}}
                     x={x} 
                     scale={{ y: 1, x: reversed ? -1 : 1 }}
-                    y={250} 
+                    y={250}
                     interactive={true}
                     onPointerDown={() => console.log('Кликнули')} />
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,11 @@ export default function Home() {
   const pixiContainer = useRef<HTMLDivElement>(null);
 
   return (
-    <div className="w-full h-full" ref={pixiContainer} >
+    <div className="w-full h-full border-4" ref={pixiContainer} >
       <Application resizeTo={pixiContainer} >
-        <CatSprite /> 
+          <pixiContainer x={250}>
+              <CatSprite />
+          </pixiContainer>
       </Application>
     </div>
   );
